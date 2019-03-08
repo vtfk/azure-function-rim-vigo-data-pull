5912052add017734bfc64a8d467b4e02262286c8
maccyber <jonas.enge@gmail.com>
2019-03-08 17:38:55 +0100

    Fix

--- pull-timed-trigger/index.js
+++ pull-timed-trigger/index.js
@@ -25,10 +25,10 @@ async function parseData (data = {}, context) {
     documents.forEach(document => {
       context.log(`Data funnet: ${document.Fornavn} ${document.Etternavn}`)
       const { Dokumentfil, ...message } = document // Removes base64 file since message queue is max 64kb
-      
+
       // Add message to queue
       context.bindings.vigoQueue.push(message)
-      
+
       // Add file to blob
       const { Dokumentfil: file, DokumentId: id } = document
       context.bindings.vigoBlob[id] = file
